4514d97412025c3bc8501f05d69859361996fbeb
jithin-space <jithinthankachan2600@gmail.com>
2024-07-30 12:19:45 +0530

    Green: Implementation for two numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,5 +1,12 @@
 function add(numbers) {
     // fail first
+    if (numbers == '') {
+        return 0;
+    } else {
+        const numberArray = numbers.split(',');
+        const sum = numberArray.reduce((agg, num) => agg + Number(num), 0);
+        return sum;
+    }
     return numbers == '' ? 0 : Number(numbers);
 }
 
