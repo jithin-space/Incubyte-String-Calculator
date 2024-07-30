a3d231e279071cc09461c0990a3e838ce36b061e
jithin-space <jithinthankachan2600@gmail.com>
2024-07-30 12:09:33 +0530

    RED: Test for single number should return the number

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -3,3 +3,7 @@ const add = require('./stringCalculator');
 test('empty string should return 0' , () => {
     expect(add('')).toBe(0);
 });
+
+test('single number should return the number', () => {
+    expect(add('2')).toBe(2);
+})
